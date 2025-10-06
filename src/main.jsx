b5ac53b5f62c53a72f7ddbdc83909f1577b5d53d
EVasiloudes <39211715+EVasiloudes@users.noreply.github.com>
2025-10-06 17:53:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Add robots noindex meta tag during development
-if (__NO_INDEX__) {
-  const meta = document.createElement('meta');
-  meta.name = 'robots';
-  meta.content = 'noindex, nofollow';
-  document.head.appendChild(meta);
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
